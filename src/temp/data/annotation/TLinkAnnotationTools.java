c3cb56c4fb7525b05d00d299cea792a46d200882
Bill McDowell <forkunited@gmail.com>
2014-03-18 16:25:55 -0400

    ...

--- src/temp/data/annotation/TLinkAnnotationTools.java
+++
@@ -1,9 +0,0 @@
-package temp.data.annotation;
-
-import ark.data.annotation.AnnotationTools;
-
-public abstract class TLinkAnnotationTools<L> extends AnnotationTools<TLinkDatum<L>, L> {
-	/* FIXME: Add extraction methods */
-
-	
-}
