f244a2ba548b90f0a9ccff3ed3b49253530e9ca4
Bill McDowell <forkunited@gmail.com>
2014-03-18 16:25:17 -0400

    Fixed tlink datum stuff

--- src/temp/data/annotation/TLinkAnnotationTools.java
+++ src/temp/data/annotation/TLinkAnnotationTools.java
@@ -5,4 +5,5 @@ import ark.data.annotation.AnnotationTools;
 public abstract class TLinkAnnotationTools<L> extends AnnotationTools<TLinkDatum<L>, L> {
 	/* FIXME: Add extraction methods */
 
+	
 }
--- src/temp/data/annotation/TLinkDatum.java
+++ src/temp/data/annotation/TLinkDatum.java
@@ -2,6 +2,7 @@ package temp.data.annotation;
 
 import temp.data.annotation.timeml.TLink;
 import ark.data.annotation.Datum;
+import ark.data.annotation.nlp.TokenSpan;
 
 public class TLinkDatum<L> extends Datum<L> {
 	protected TLink tlink;
@@ -13,5 +14,64 @@ public class TLinkDatum<L> extends Datum<L> {
 	
 	public TLink getTLink() {
 		return this.tlink;
+	}
+	
+	public static Tools<TLink.TimeMLRelType> getTimeMLRelTypeTools() {
+		return new Tools<TLink.TimeMLRelType>() {
+			@Override
+			public TLink.TimeMLRelType labelFromString(String str) {
+				return TLink.TimeMLRelType.valueOf(str);
+			}
+		};
+	}
+	
+	private static abstract class Tools<L> extends Datum.Tools<TLinkDatum<L>, L> {
+		public Tools() {
+			super();
+			
+			this.addTokenSpanExtractor(new TokenSpanExtractor<TLinkDatum<L>, L>() {
+				@Override
+				public String toString() {
+					return "SourceTokenSpan";
+				}
+				
+				@Override
+				public TokenSpan[] extract(TLinkDatum<L> tlinkDatum) {
+					return new TokenSpan[] { 
+						tlinkDatum.getTLink().getSource().getTokenSpan()
+					};
+				}
+			});
+			
+			this.addTokenSpanExtractor(new TokenSpanExtractor<TLinkDatum<L>, L>() {
+				@Override
+				public String toString() {
+					return "TargetTokenSpan";
+				}
+				
+				@Override
+				public TokenSpan[] extract(TLinkDatum<L> tlinkDatum) {
+					return new TokenSpan[] { 
+						tlinkDatum.getTLink().getTarget().getTokenSpan()
+					};
+				}
+			});
+			
+			this.addTokenSpanExtractor(new TokenSpanExtractor<TLinkDatum<L>, L>() {
+				@Override
+				public String toString() {
+					return "SignalTokenSpan";
+				}
+				
+				@Override
+				public TokenSpan[] extract(TLinkDatum<L> tlinkDatum) {
+					if (tlinkDatum.getTLink().getSignal() == null)
+						return null;
+					return new TokenSpan[] { 
+						tlinkDatum.getTLink().getSignal().getTokenSpan()
+					};
+				}
+			});
+		}
 	}
 }
