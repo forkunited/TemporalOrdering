bce8f571ea3f919837e2647ab5016c079b876e77
Bill McDowell <forkunited@gmail.com>
2014-03-18 23:48:19 -0400

    Changed feature representation

--- src/temp/data/feature/FeatureTLinkEventTense.java
+++ src/temp/data/feature/FeatureTLinkEventTense.java
@@ -12,42 +12,73 @@ import ark.data.feature.Feature;
  * @author Bill McDowell
  */
 public class FeatureTLinkEventTense<L> extends Feature<TLinkDatum<L>, L>{
+
+	@Override
+	public Map<Integer, Double> computeVector(TLinkDatum<L> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGenericName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGenericShortName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getParameterNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getParameterShortNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getParameterValue(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int getVocabularySize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected String getVocabularyTerm(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean init(List<TLinkDatum<L>> arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected Feature<TLinkDatum<L>, L> makeInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean setParameterValue(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
-	public FeatureTLinkEventTense() {
-		
-	}
-	
-	@Override
-	public Feature<TLinkDatum<L>, L> clone() {
-		return new FeatureTLinkEventTense<L>();
-	}
 
-	@Override
-	public Map<Integer, Double> computeVector(TLinkDatum<L> datum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String> getNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void init(List<TLinkDatum<L>> data) {
-		return;
-	}
-
-	@Override
-	public void init(String initStr) {
-		return;
-	}
-
-	@Override
-	public String toString(boolean withInit) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
