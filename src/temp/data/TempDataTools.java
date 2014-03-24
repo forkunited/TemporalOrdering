d31f0580c20f26d32f8a7728c137e9141c3d5780
Bill McDowell <forkunited@gmail.com>
2014-03-24 16:00:25 -0400

    ...

---
+++ src/temp/data/TempDataTools.java
@@ -0,0 +1,30 @@
+package temp.data;
+
+import java.io.File;
+
+import temp.util.TempProperties;
+import ark.data.DataTools;
+import ark.util.OutputWriter;
+
+public class TempDataTools extends DataTools {
+	private TempProperties properties;
+	public TempDataTools(OutputWriter outputWriter, TempProperties properties) {
+		super(outputWriter);
+		
+		this.properties = properties;
+		this.addPath("CregCmd", new Path("CregCmd", properties.getCregCommandPath()));
+	}
+	
+	public Path getPath(String name) {
+		if (name == null)
+			return null;
+		if (!name.startsWith("CregModel"))
+			return super.getPath(name);
+		
+		String modelName = name.substring("CregModel/".length());
+		String modelPath = (new File(this.properties.getCregDataDirPath(), modelName)).getAbsolutePath();
+		return new Path(name, modelPath);
+		
+	}
+
+}
