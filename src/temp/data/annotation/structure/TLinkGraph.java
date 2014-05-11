5bac4ce67030eb31de9675c8b0f0f8c2214f16a7
Bill McDowell <forkunited@gmail.com>
2014-05-10 23:32:51 -0400

    ...

--- src/temp/data/annotation/structure/TLinkGraph.java
+++ src/temp/data/annotation/structure/TLinkGraph.java
@@ -228,11 +228,11 @@ public class TLinkGraph<L> extends DatumStructure<TLinkDatum<L>, L> {
 			
 			// Find value to scale datum's by in case of large coefficients that
 			// cause ILP solver instability
-			double maxLabelMagnitude = Double.NEGATIVE_INFINITY;
+			/*double maxLabelMagnitude = Double.NEGATIVE_INFINITY;
 			for (Map<L, Double> datumValue : scoredDatumLabels.values()) {
 				for (Double value : datumValue.values())
 					maxLabelMagnitude = Math.max(maxLabelMagnitude, Math.abs(value));
-			}
+			}*/
 			
 			// Objective function and rule based constraints
 			Linear objective = new Linear();
@@ -245,7 +245,7 @@ public class TLinkGraph<L> extends DatumStructure<TLinkDatum<L>, L> {
 				
 				double minValue = Double.MAX_VALUE;
 				for (Entry<L, Double> labelEntry : labelValues.entrySet()) {
-					double value = labelEntry.getValue()/maxLabelMagnitude; // Scale value
+					double value = labelEntry.getValue();//maxLabelMagnitude; // Scale value
 					objective.add(value, tlinkVarPrefix + labelEntry.getKey());
 					minValue = Math.min(minValue, value);
 				}
